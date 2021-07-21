afe643c7b85959a378ecc80d079f1754d0e20888
Conti Matteo <contimatteo1505@gmail.com>
2021-07-21 19:32:35 +0200

    WIP: cvxopt formulation in progress

--- src/core/svm.py
+++ src/core/svm.py
@@ -1,3 +1,6 @@
+import cvxopt
+import numpy as np
+
 ###
 
 
@@ -7,7 +10,59 @@ class SVM():
 
     #
 
-    def fit(self, X_train, Y_train):
+    def __cvxopt_formulation(self, X, Y):
+        """
+        Lagrangian Formulation (MIN) \\
+        `L(λ, w, b) = 1/2 * ||w||^2 + ∑ λi * (yi * (w * xi + b) - 1)`
+
+        Dual of Lagrangian Formulation (MAX) \\
+        `F(λ) = ∑ λi - 1/2 * (∑ ∑ λi * λi * yi * yj * xi * xj)`
+        
+        CVXopt Formulation (MIN) \\
+        `1/2 * (x.T * P * x) + (q.T * x)` \\
+        `Gx ≤ h` \\
+        `Ax = b`
+
+        in order to obtain a MIN problem, we start from the dual
+        and we multiply by -1 the entire objective function.
+
+        CVXopt Formulation Applied (MIN) \\
+        `1/2 * (λ.T * (X * Y) * λ) - (I.T * λ)` \\
+        `(-1 * I * λi) ≤ 0` \\
+        `∑ yi * λi = 0`
+
+        CVXopt Variables:
+         - P = (X * Y) = Hessian
+         - q = -I.T
+         - G = -I
+         - h = 0 (vector)
+         - A = Y
+         - b = 0 (number)
+        """
+
+        N = X.shape[0]
+
+        P_raw = np.outer(np.dot(X, X.T), np.dot(Y, Y.T))
+        q_raw = -1 * np.identity(N).T
+        G_raw = -1 * np.identity(N)
+        h_raw = np.zeros(N)
+        A_raw = Y
+        b_raw = np.zeros(N)
+
+        P = cvxopt.matrix(P_raw)
+        q = cvxopt.matrix(q_raw)
+        G = cvxopt.matrix(G_raw)
+        h = cvxopt.matrix(h_raw)
+        A = cvxopt.matrix(A_raw)
+        b = cvxopt.matrix(b_raw)
+
+        return (P, q, G, h, A, b)
+
+    def fit(self, X, Y):
+        # solve QP problem
+        P, q, G, h, A, b = self.__cvxopt_formulation(X, Y)
+        solution = cvxopt.solvers.qp(P, q, G, h, A, b)
+
         pass
 
     def predict(self, X_test):
